4539a3b0faf950a744e7a7f7bfcef9e29d7cd35a
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-08-19 03:38:29 +0800

    feat: Added an initial main function that is to be used with the function

---
+++ main.py
@@ -0,0 +1,10 @@
+def main(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/0.12/api/#flask.Flask.make_response>.
+    """
+    return 'Hello, World!'
