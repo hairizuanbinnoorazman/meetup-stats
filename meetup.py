fd7794b1b7400ea0134746fa3d2188d4893dacfb
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-08-20 00:30:28 +0800

    feat: Added a basic function to do some quick analysis on rsvps coming

---
+++ meetup.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+BASE_API = "https://api.meetup.com/"
+
+
+def get_rsvps(group_name, event_id):
+    api_endpoint = "%s%s/events/%s/rsvps" % (BASE_API, group_name, event_id)
+    req = requests.get(api_endpoint)
+    data = req.json()
+    filtered_data = [{'time': datetime.fromtimestamp(
+        row['updated']/1000), 'guests': row['guests'], 'attendees': row['guests']+1} for row in data if row['response'] == 'yes']
+    df = pd.DataFrame(filtered_data)
+    a = df.plot(x='time', y='attendees')
+    plt.savefig('test.png')
