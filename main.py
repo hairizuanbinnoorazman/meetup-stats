bd80be92a2fe14232f1395eee444e87178f4c75f
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-08-22 23:07:53 +0800

    fix: Rearrange code base to have slack creds at the top. At the same tiem, also wrap main error prone section with exception handling to return back status of code

--- main.py
+++ main.py
@@ -19,22 +19,40 @@ def main(request):
     group_name = ""
     event_id = ""
 
-    text_value = request.form.get('text')
-    logging.info(text_value)
-    if text_value is not None or text_value == "":
-        split_text = text_value.split(" ")
-        if len(split_text) == 1:
-            group_name = split_text[0]
-            event_id = meetup.get_latest_upcoming_event(group_name)
-        else:
-            group_name = split_text[0]
-            event_id = split_text[1]
-
+    # Retrieve creds
     client = storage.Client()
     bucket = client.get_bucket('gcpug-meetup-files')
     blob = bucket.get_blob('config/config.json')
     keys = blob.download_as_string()
     keys_json = json.loads(keys)
+
+    # Retrieve slack channel id
+    slack_token = keys_json['slack_token']
+    slack_channel_name = keys_json['slack_channel_name']
+    channel_id = slack.get_channel_list(slack_token, slack_channel_name)
+
+    text_value = request.form.get('text')
+    logging.info("Text_Value: %s" % (text_value))
+    try:
+        if text_value is not None or text_value == "":
+            split_text = text_value.split(" ")
+            if len(split_text) == 1:
+                group_name = split_text[0]
+                event_id = meetup.get_latest_upcoming_event(group_name)
+            else:
+                group_name = split_text[0]
+                event_id = split_text[1]
+    except ValueError as e:
+        logging.error(e)
+        logging.error(e)
+        slack.send_text_to_channel(
+            slack_token, channel_id, "Group name is unavailable. Please check again")
+        return "error"
+    except Exception as e:
+        logging.error(e)
+        slack.send_text_to_channel(
+            slack_token, channel_id, "Error in retrieving stats. Check logs")
+        return "error"
 
     # TODO: Move default channel to a config file
     if group_name == "":
@@ -43,9 +61,6 @@ def main(request):
         event_id = "251921227"
     meetup.get_rsvps(group_name, event_id)
 
-    slack_token = keys_json['slack_token']
-    slack_channel_name = keys_json['slack_channel_name']
-    channel_id = slack.get_channel_list(slack_token, slack_channel_name)
     slack.upload_image_to_channel(slack_token, channel_id,
                                   "/tmp/test.png")
 
